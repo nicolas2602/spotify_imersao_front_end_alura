76d2ea474e4530f1cd7a91205b6b270904a4310d
Nicolas Hiroki Yonekawa <69517285+nicolas2602@users.noreply.github.com>
2024-01-27 15:26:31 -0300

    Aula 4 parte 1

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+    Passo 1: Instalar o JSON-SERVER (versão antiga e confiável): 
+    npm install -g json-server@0.17.4
+
+    Passo 2: Abrir o PowerShell e executar como administrador:
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+
+    Passo 3: Digite "A" para sim e dar enter
+
+    Passo 4: Executar o arquivo .json
+    json-server --watch api-artist/artist.json
+
+    Passo 5: Após a executar, clique na url da API:
+    http://localhost:3000/artists
+
+*/ 
+
+// DOM  - representação de uma árvore
