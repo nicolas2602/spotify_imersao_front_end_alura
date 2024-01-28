78ca2b969c2b38049c91b0644a7473ae62d642a6
Nicolas Hiroki Yonekawa <69517285+nicolas2602@users.noreply.github.com>
2024-01-28 16:46:26 -0300

    aula 4 parte 2

--- script.js
+++ script.js
@@ -16,3 +16,47 @@
 */ 
 
 // DOM  - representação de uma árvore
+/* Pega o ID */
+const searchInput = document.getElementById('search-input'); 
+const resultArtist = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlists')
+
+// Consumir a API JSON
+function requestApi(searchTerm){
+    // Promisse = Programação assíncrona
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
+
+function displayResults(result) {
+    resultPlaylist.classList.add('hidden');
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtist.classList.remove('hidden');
+}
+
+// Receber um evento
+document.addEventListener('input', function() {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === ''){
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+        return; // Parar a aplicação
+    }
+
+    requestApi(searchTerm);
+});
+
+/* Pega Classe */
+// const searchInput2 = document.querySelector(".cards"); 
+
+/* Pega todas as Classe por Array*/
+// const searchInput3 = document.querySelectorAll(".cards"); 
